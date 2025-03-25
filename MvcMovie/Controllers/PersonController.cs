4dd7d7e9e962b9abe5e4592377dd65ad508538d9
NguyenChiThanh14 <thanhnguyen141003a@gmail.com>
2025-03-25 23:48:04 +0700

    code phan trang

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -10,6 +10,8 @@ using MvcMovie.Models;
 using MvcMovie.Models.Process;
 using System.Data;
 using OfficeOpenXml;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace MvcMovie.Controllers
 {
@@ -23,12 +25,29 @@ namespace MvcMovie.Controllers
             _context = context;
         }
 
+        public async Task<IActionResult> Index(int? page, int? PageSize)
+        {
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value="3", Text="3"},
+                new SelectListItem() { Value="5", Text="5"},
+                new SelectListItem() { Value="10", Text="10"},
+                new SelectListItem() { Value="15", Text="15"},
+                new SelectListItem() { Value="25", Text="25"},
+                new SelectListItem() { Value="50", Text="50"},
+            };
+            int pagesize = (PageSize ?? 3);
+            ViewBag.psize = pagesize;
+            var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
+            return View(model);
+        }
+
         // GET: Person
-        public async Task<IActionResult> Index()
+        /* public async Task<IActionResult> Index()
         {
             return View(await _context.Person.ToListAsync());
         }
-
+ */
         public IActionResult Download()
         {
             //Name the file when download
--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74cdea597640a76a072d58134efc02368e40a8c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6c4536de195f38d103135eb0e0fdf4741b6208")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
