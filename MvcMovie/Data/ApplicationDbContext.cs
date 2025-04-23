e956e48b559c3049a9b162fcc8b46067cb8b74de
NguyenChiThanh14 <thanhnguyen141003a@gmail.com>
2025-04-23 14:20:19 +0700

    identity

--- MvcMovie/Data/ApplicationDbContext.cs
+++ MvcMovie/Data/ApplicationDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
+
 namespace MvcMovie.Data
 {
     using Microsoft.EntityFrameworkCore;
     using MvcMovie.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6c4536de195f38d103135eb0e0fdf4741b6208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd7d7e9e962b9abe5e4592377dd65ad508538d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
